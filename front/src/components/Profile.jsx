c81bb9884275196619cd99eddcd9eee395854ffe
adriandinug <adrian.dinu@gdm.ro>
2022-11-23 17:30:40 +0200

    added markdown component

--- front/src/components/Profile.jsx
+++ front/src/components/Profile.jsx
@@ -1,10 +1,46 @@
+import { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+
 function Profile({ user }) {
+  const [note, setNote] = useState(`
+  # Welcome to the Student Notes App!
+  ## Second heading
+  ### Third heading
+  #### Fourth heading
+  ##### Fifth heading
+  paragraph
+
+  **bold**
+
+  *italic*
+
+  ***bold and italic***
+
+  > blockquote asdadas
+
+  \`code code code\`
+
+  * List1
+  * List 2
+  * List 3
+  
+  1. List1
+  2. List2
+  3. List3
+
+  ---
+
+  test
+
+  test
+  `);
+
   const logout = () => {
     localStorage.removeItem('user');
     window.location.reload();
   };
   return (
-    <div className='profile'>
+    <div className='profile' style={{ paddingBottom: '100px' }}>
       {user ? (
         <>
           <h2>This is my profile</h2>
@@ -22,6 +58,21 @@ function Profile({ user }) {
       ) : (
         <h2>You are not logged in</h2>
       )}
+      <div
+        className='markdown'
+        style={{ marginTop: '30px', display: 'flex', gap: '50px' }}
+      >
+        <textarea
+          name='mark'
+          id=''
+          cols='40'
+          rows='20'
+          style={{ resize: 'none' }}
+          value={note}
+          onChange={(e) => setNote(e.target.value)}
+        ></textarea>
+        <ReactMarkdown className='markdown-wrapper' children={note}></ReactMarkdown>
+      </div>
     </div>
   );
 }
